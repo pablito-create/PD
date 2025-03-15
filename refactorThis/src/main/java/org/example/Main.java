415edf8794fc61a99ab3226c9960077dcce4e516
Pablo <pablo.barbero2@educa.madrid.org>
2025-03-15 18:17:06 +0100

    Actualizar clase Main

--- refactorThis/src/main/java/org/example/Main.java
+++ refactorThis/src/main/java/org/example/Main.java
@@ -1,52 +1,14 @@
-//Clase persona con atributos básicos
-
-class Persona {
-    static String nombre = "SinNombre";
-    static int edad = 0;
-    static double salario = 0.0;
-
-    static void datos(String n, int e, double s) {
-        nombre = n;
-        edad = e;
-        salario = s;
-    }
-
-    static void mostrar() {
-        System.out.println("Nombre: " + nombre);
-        System.out.println("Edad: " + edad);
-        System.out.println("Salario: " + salario);
-    }
-}
-
-class B {
-    void calcularBono() {
-        double bono = 0;
-        if (A.edad > 50) {
-            bono = A.salario * 0.10;
-        } else {
-            bono = A.salario * 0.05;
-        }
-        System.out.println("Bono: " + bono);
-    }
-}
-
-class C {
-    void imprimirSaludo() {
-        if (A.edad < 18) {
-            System.out.println("Hola, joven " + A.nombre);
-        } else {
-            System.out.println("Hola, señor/a " + A.nombre);
-        }
-    }
-}
-
+/*
+ * Actualizar la clase Main
+ * 
+ */
 public class Main {
     public static void main(String[] args) {
-        A.datos("Juan Perez", 45, 50000);
-        A.mostrar();
-        B b = new B();
-        b.calcularBono();
-        C c = new C();
-        c.imprimirSaludo();
+        Persona.datos("Juan Perez", 45, 50000);
+        Persona.mostrar();
+        calcularBono bono1= new calcularBono();
+        bono1.calcularBono();
+        Saludar saludito = new Saludar();
+        saludito.imprimirSaludo();
     }
 }
