ac5f4383b7543bcaa9d0d2eca959fa20f2d01eb4
Pablo <pablo.barbero2@educa.madrid.org>
2025-03-15 18:03:20 +0100

    Actualización de la clase CalcularBono

---
+++ CalcularBono.java
@@ -0,0 +1,19 @@
+/*
+ * Encapsular método 
+ * Encapsular atributo
+ * @author:Pablo
+ */
+
+public class calcularBono{
+
+    private void calcularBono() {
+        private double bono = 0;
+        if (Persona.edad > 50) {
+            bono = Persona.salario * 0.10;
+        } else {
+            bono = Persona.salario * 0.05;
+        }
+        System.out.println("Bono: " + bono);
+    }
+
+}
